4c821993f733558f11d7b1483ac91cf228416ec0
Cosmic Muffin <dmgordon95@yahoo.com>
2020-02-17 03:05:08 -0600

    added core game logic functions

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* CS 4328 Operating Systems
+ * Programming Assignment 1
+ * 
+ * Dustin Gordon & 
+ * Jose 
+ */
+#include <iostream>
+#include <stdio.h>
+#include <pthread.h>
+using namespace std;
+
+
+int main(){
+
+	return 0;
+}
+
